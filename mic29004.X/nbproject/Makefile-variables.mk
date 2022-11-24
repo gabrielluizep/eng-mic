6a2c48bdaf401c46612d7aa7b0cb0127d34b861d
gabrielluizep <gabrielluizep@hotmail.com>
2022-11-24 10:35:06 -0300

    :technologist: added base project

---
+++ mic29004.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mic29004.X.production.obj
+CND_ARTIFACT_PATH_default=dist/default/production/mic29004.X.production.obj
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mic29004.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mic29004.x.tar
+# programming configuration
+CND_ARTIFACT_DIR_programming=dist/programming/production
+CND_ARTIFACT_NAME_programming=mic29004.X.production.obj
+CND_ARTIFACT_PATH_programming=dist/programming/production/mic29004.X.production.obj
+CND_PACKAGE_DIR_programming=${CND_DISTDIR}/programming/package
+CND_PACKAGE_NAME_programming=mic29004.x.tar
+CND_PACKAGE_PATH_programming=${CND_DISTDIR}/programming/package/mic29004.x.tar
